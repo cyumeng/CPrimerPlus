ed9711b3fef9439d21c66ff8aa52f06fd95233b6
cyumeng <cyumeng@gmail.com>
2015-03-30 23:55:44 +0700

    Chapter4 2015/03/30

---
+++ plist.4.12.c
@@ -0,0 +1,16 @@
+/* mismatched floating-point conversions */
+#include <stdio.h>
+int main(void)
+{
+	float n1 = 3.0;
+	double n2 = 3.0;
+	long n3 = 2000000000;
+	long n4 = 1234567890;
+
+	printf("%.1e %.1e %.1e %.1e\n", n1, n2, n3, n4);
+	printf("%ld %ld\n", n3, n4);
+	printf("%ld %ld %ld %ld\n", n1, n2, n3, n4);
+	printf("%.1f %.1e %ld %ld\n", n1, n2, n3, n4);	// Correct specifiers
+
+	return 0;
+}
