43693268d882af917dffc3eca132cf8242f62c07
cyumeng <cyumeng@gmail.com>
2015-04-01 23:42:57 +0700

    Chapter5 2015/04/01

---
+++ plist.5.15.c
@@ -0,0 +1,22 @@
+/* defines a function with an argument */
+#include <stdio.h>
+void pound(int n);			// ANSI function prototype declaration
+int main(void)
+{
+	int times = 5;
+	char ch = '!';			// ASCII code is 33
+	float f = 6.0f;
+
+	pound(times);			// int argument
+	pound(ch);				// same as pound((int)ch)
+	pound(f);				// same as pound((int)f)
+
+	return 0;
+}
+
+void pound(int n)			// ANSI-style function header
+{
+	while (n-- > 0)
+		printf("#");
+	printf("\n");
+}
